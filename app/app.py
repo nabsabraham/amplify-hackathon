f179e4c74d32d005fd0cfc4abe7e6ac75c774383
nabsabraham <nabila.abraham@gmail.com>
2020-10-29 09:17:06 -0400

    bootstrap version

--- app/app.py
+++ app/app.py
@@ -3,19 +3,31 @@ from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route("/recommend")
+def recommend():
+    return render_template('recommender.html')
 
 @app.route("/upload")
-def sign_up():
+def upload():
     if request.method == "POST":
+        if request.files:
+            image = request.files["image"]
+            print(image)
+            #return redirect(request.url)
+    return render_template("upload.html", name='Nabila')
 
-        req = request.form
-        print(req)
+    #if request.method == "POST":
+        #req = request.form
+        #print(req)
+        #return redirect(request.url)
 
-        return redirect(request.url)
+    #return render_template("upload.html")
 
-    return render_template("upload.html")
 
+
+    return render_template("public/upload_image.html")
 if __name__ == "__main__":
     app.run()
