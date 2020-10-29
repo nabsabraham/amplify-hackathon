a4242c29fc2037a305b93a75eae6125c8ec4b010
nabsabraham <nabila.abraham@gmail.com>
2020-10-29 01:29:19 -0400

    home page with logo, uploader with buttons that dont work

--- app/app.py
+++ app/app.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return render_template('index.html')
+
+@app.route("/upload")
+def sign_up():
+    if request.method == "POST":
+
+        req = request.form
+        print(req)
+
+        return redirect(request.url)
+
+    return render_template("upload.html")
+
+if __name__ == "__main__":
+    app.run()
