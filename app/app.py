05f97fbeb1541d4d7c9a6ff38c98b3eb11df8968
nabsabraham <nabila.abraham@gmail.com>
2020-10-30 00:11:36 -0400

    images are now showing using only home.html

--- app/app.py
+++ app/app.py
@@ -1,18 +1,25 @@
 import os
 from werkzeug.utils import secure_filename
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, abort
 
 app = Flask(__name__)
 
-UPLOADS_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static\\images')
+app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static', 'uploads')
+app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'gif'])
 
 @app.route("/")
 def home():
     return render_template('home.html')
 
-@app.route("/recommend")
-def recommend():
-    return render_template('recommender.html')
+@app.route('/', methods=['POST'])
+def upload_files():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        uploaded_file.save(path)
+    image_url = url_for('static', filename='uploads/'+str(filename))
+    return render_template('home.html', filename=filename, image_url=image_url)
 
 @app.route("/upload")
 def upload():
@@ -27,18 +34,21 @@ def upload():
 @app.route("/form", methods=['POST'])
 def form():
     if request.method == 'POST':
-        image_name = request.form['filename']
-        if image_name != '':
-            path = os.path.join(UPLOADS_PATH, secure_filename(image_name))
-            #image_name.save(path)
-        return render_template('form.html', image_name=image_name, path=path)
-    #if request.method == "POST":
-        #req = request.form
-        #print(req)
-        #return redirect(request.url)
+        if 'image_file' not in request.files:
+            return 'there is no image_file in form!'
+        image_file = request.files['image_file']
+        path = os.path.join(app.config['UPLOAD_FOLDER'], image_file.filename)
+        image_file.save(path)
+        return render_template('form.html', path=path, filename=image_file)
 
-    #return render_template("upload.html")
+@app.route('/display/<filename>')
+def display_image(filename):
+	print('display_image filename: ' + filename)
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
+@app.route("/recommend")
+def recommend():
+    return render_template('recommender.html')
 
 
 if __name__ == "__main__":
