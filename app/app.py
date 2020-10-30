dfba263c2c5e23d2f7e79056b967e003569c4c58
nabsabraham <nabila.abraham@gmail.com>
2020-10-29 22:29:46 -0400

    getting local path to img - now what?

--- app/app.py
+++ app/app.py
@@ -1,6 +1,10 @@
+import os
+from werkzeug.utils import secure_filename
 from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
+
+UPLOADS_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static\\images')
 
 @app.route("/")
 def home():
@@ -17,8 +21,17 @@ def upload():
             image = request.files["image"]
             print(image)
             #return redirect(request.url)
-    return render_template("upload.html", name='Nabila')
+    return render_template("upload.html")
 
+# we are posting to this page from the /home page
+@app.route("/form", methods=['POST'])
+def form():
+    if request.method == 'POST':
+        image_name = request.form['filename']
+        if image_name != '':
+            path = os.path.join(UPLOADS_PATH, secure_filename(image_name))
+            #image_name.save(path)
+        return render_template('form.html', image_name=image_name, path=path)
     #if request.method == "POST":
         #req = request.form
         #print(req)
@@ -28,6 +41,5 @@ def upload():
 
 
 
-    return render_template("public/upload_image.html")
 if __name__ == "__main__":
     app.run()
