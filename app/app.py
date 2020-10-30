5c91d2ea5e56c3779b16471524026f4c510abce2
nabsabraham <nabila.abraham@gmail.com>
2020-10-30 02:40:54 -0400

    pretty front end, minimal text from onclick func

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import os
+import json
 from werkzeug.utils import secure_filename
 from flask import Flask, render_template, url_for, request, redirect, abort
 
@@ -19,32 +20,10 @@ def upload_files():
         path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         uploaded_file.save(path)
     image_url = url_for('static', filename='uploads/'+str(filename))
-    return render_template('home.html', filename=filename, image_url=image_url)
 
-@app.route("/upload")
-def upload():
-    if request.method == "POST":
-        if request.files:
-            image = request.files["image"]
-            print(image)
-            #return redirect(request.url)
-    return render_template("upload.html")
+    return render_template('home.html', filename=filename, image_url=image_url,
+                           produce='banana')
 
-# we are posting to this page from the /home page
-@app.route("/form", methods=['POST'])
-def form():
-    if request.method == 'POST':
-        if 'image_file' not in request.files:
-            return 'there is no image_file in form!'
-        image_file = request.files['image_file']
-        path = os.path.join(app.config['UPLOAD_FOLDER'], image_file.filename)
-        image_file.save(path)
-        return render_template('form.html', path=path, filename=image_file)
-
-@app.route('/display/<filename>')
-def display_image(filename):
-	print('display_image filename: ' + filename)
-	return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 @app.route("/recommend")
 def recommend():
